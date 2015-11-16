f9a1f48c52d6c88d7eceb11895e497121f9fbe41
Bijil <bijilap@gmail.com>
2015-11-15 22:15:52 -0800

    Adding logic to get top colors. Incomplete

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-from sklearn.cluster import KMeans
-import cv2
-import utils
 import argparse
+from ColorDetector import ColorDetector
+
+def main():
+    detector = ColorDetector()
+    parser = argparse.ArgumentParser()
+    # --k : number of clusters, --image: image path, --debug: debug level
+    parser.add_argument("--k", nargs=1)
+    parser.add_argument("--n", nargs=1)
+    parser.add_argument("--image", nargs=1)
+    parser.add_argument("--debug", nargs=1)
+    args = parser.parse_args()
+
+    img_name = None
+    n = 4
+
+    if args.k:
+        detector.NUM_OF_CLUSTERS = int(args.k[0])
+
+    if args.image:
+        img_name = args.image[0]
+
+    if args.debug:
+        detector.log_level = int(args.debug[0])
+
+    if args.n:
+        n = int(args.n[0])
+
+    image = detector.readImage(img_name)
+    detector.getDominantColors(image, n)
+
+
+if __name__ == "__main__":
+    main()
